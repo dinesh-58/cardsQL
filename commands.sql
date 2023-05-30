0a399b6f268ff49ac3c0fa27a0d03ff3fec4c45f
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-05-30 18:01:31 +0545

    add sql constraints for front & back fields

--- commands.sql
+++ commands.sql
@@ -1,18 +1,18 @@
 -- these are for SQLite so some syntax may vary if you use a differentd RDBMS
 
 -- creating cards
- create table cards(
-            id integer primary key autoincrement,
-            front text not null,
-            back text not null,
-            direction int not null default 1 
-            check(direction >= 0 and direction <=3),
-            successfulRevisions int not null default 0 
-            check(successfulRevisions >= 0),
-            easeFactor real not null default 2.5 
-            check(easeFactor <=2.5 AND easeFactor >= 1.3),
-   			scheduledDate text not null default CURRENT_DATE
- ) strict;
+create table cards(
+    id integer primary key autoincrement,
+    front text not null check(length(front) <= 255),
+    back text not null check(length(back) <= 255),
+    direction int not null default 1
+    check(direction >= 0 and direction <=3),
+    successfulRevisions int not null default 0
+    check(successfulRevisions >= 0),
+    easeFactor real not null default 2.5
+    check(easeFactor <=2.5 AND easeFactor >= 1.3),
+    scheduledDate text not null default CURRENT_DATE
+) strict;
 
 -- direction values: 
     -- 0 : disabled
