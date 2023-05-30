952eec5d725396f5bf5a5d20ac6cc566e8af1750
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-05-30 18:46:52 +0545

    implement insertion into DB using form

--- index.php
+++ index.php
@@ -7,27 +7,53 @@
         <link href="style.css" rel="stylesheet">
     </head>
     <body>
-        <form action="" method="post">
-            <!-- maxlength is specified to set limit for varchar in database table--> 
+        <?php require_once './header.html'?>
+        <form action="" method="">
             <label>
                 Front:
-            <textarea name="card-front" id="" maxlength="280" spellcheck="true" cols="30" rows="10" placeholder="What is CSS used for?" required autofocus></textarea>
+            <textarea name="card-front" id="" maxlength="255" spellcheck="true" cols="30" rows="10" placeholder="Question. Example: What is CSS used for?" required autofocus></textarea>
             </label>
             <span>Card direction:</span>
             <!--? Can use arrow characters (not emoji) for these instead of text-->
-            <input type="radio" name="card-direction" value="forward" checked>Forward
-            <input type="radio" name="card-direction" value="backward">Backward
-            <input type="radio" name="card-direction" value="bi">Bi-directional
+            <input type="radio" name="card-direction" value="1" checked>Forward
+            <input type="radio" name="card-direction" value="3">Backward
+            <input type="radio" name="card-direction" value="2">Bi-directional
             <label>
                 Back:
-            <textarea name="card-back" id="" maxlength="280" spellcheck="true" cols="30" rows="10" placeholder="Styling our HTML" required></textarea>
+            <textarea name="card-back" id="" maxlength="255" spellcheck="true" cols="30" rows="10" placeholder="Answer. Example: Styling our HTML" required></textarea>
             </label>
-            <!--TODO use php for setting default attributes (review_date, EF, n)-->
-            <button>Submit</button>
+            <button name="submit">Submit</button>
         </form> 
 
 <?php 
-echo "testing"
+if (isset($_GET['submit'])) {
+    $front = $_GET['card-front'];
+    $back = $_GET['card-back'];
+    $direction = $_GET['card-direction'];
+
+    $pdo = new PDO('sqlite:cardsql.db');
+    
+    // we only insert values specifiable by user. the rest all have default values set in the database schema
+    $query = 'insert into cards(front, back, direction) values(?, ?, ?);';
+
+    // prepare statement
+    $statement = $pdo->prepare($query);
+    $statement->bindValue(1, $front, PDO::PARAM_STR);
+    $statement->bindValue(2, $back,  PDO::PARAM_STR);
+    $statement->bindValue(3, $direction, PDO::PARAM_INT);
+
+    if ($statement->execute()) {
+        echo 'Card created!';}
+/*
+    // TODO create custom-functions.php file. displayMessage(success, message)  
+        // if success, echo html div with green background & tick
+        // else use red background & cross
+    // after creating, replace the above echo statement with these 2 lines below
+    
+        displayMessage(true, 'Card created!');
+    } else displayMessage(false, 'Error creating card');
+*/
+}
 ?>
 
     </body>
