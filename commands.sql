94ed26ee0c02499cd9e806a686a5a9a2738fd642
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-05-31 16:40:09 +0545

    add sql constraint for scheduledDate

--- commands.sql
+++ commands.sql
@@ -5,14 +5,20 @@ create table cards(
     id integer primary key autoincrement,
     front text not null check(length(front) <= 255),
     back text not null check(length(back) <= 255),
-    direction int not null default 1
-    check(direction >= 0 and direction <=3),
-    successfulRevisions int not null default 0
-    check(successfulRevisions >= 0),
-    easeFactor real not null default 2.5
-    check(easeFactor <=2.5 AND easeFactor >= 1.3),
-    interval int not null default 1 check(interval >=0),
-    scheduledDate text not null default CURRENT_DATE
+    direction int not null default 1 check(
+        direction >= 0
+        and direction <= 3
+    ),
+    successfulRevisions int not null default 0 check(successfulRevisions >= 0),
+    easeFactor real not null default 2.5 check(
+        easeFactor <= 2.5
+        AND easeFactor >= 1.3
+    ),
+    interval int not null default 1 check(interval >= 0),
+    scheduledDate text not null default CURRENT_DATE check (
+        length(scheduledDate) <= 10
+        AND DATE(scheduledDate, '+0 days') IS scheduledDate
+    )
 ) strict;
 
 -- direction values: 
