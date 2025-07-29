136cd54a093315e2529caedc0642ba9b80dcec85
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2025-07-29 22:23:33 +0545

    create bootstrap.php, move connect_db there

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,11 @@
 <?php
+define("BASE_DIR", __DIR__);
+
 function connect_db()
 {
     try {
-        $pdo = new PDO("sqlite:cardsql.db");
-        // Enable foreign key constraints for this connection
+        $pdo = new PDO("sqlite:" . BASE_DIR . "/cardsql.db");
+        // Enables foreign key constraints for this connection.
         // this needs to be set for every connection so always use this function to connect to db
         $pdo->exec("PRAGMA foreign_keys = ON;");
         return $pdo;
