a926ed8fd2a61dbae7f29983412d2885009b7b6c
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2025-07-29 21:15:07 +0545

    add basic Card class definition
    not using for now. might do in future

---
+++ inc/classes/Card.php
@@ -0,0 +1,19 @@
+<?php
+// TODO: maybe use this (in getCards, SRS, rateCard)
+class Card
+{
+    public int $id;
+    public string $front;
+    public string $back;
+    // maybe: set enum here for direction?
+    public string $direction;
+    public int $successfulRevisions;
+    public float $easeFactor;
+    public int $interval;
+    public string $scheduledDate;
+    public int $folder_id;
+
+    // TODO: implement methods for:
+    // - updating this card in db?
+    // - srs + update?
+}
